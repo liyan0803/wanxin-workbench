9fc4914d5ae5420a282aedffc44b3d47a8135406
liyan0803 <leelillian@163.com>
2026-09-21 21:47:55 +0800

    add sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='wanxin-mario-v7';
+const C='wanxin-mario-v8';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS).catch(()=>{})).then(()=>self.skipWaiting()));
